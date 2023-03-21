6ccb13938d5155e36117a6777590cc817bf37e0b
anna-braga-jesus <annajesus.nave@gmail.com>
2023-03-21 07:13:28 -0300

    add multiplication table

---
+++ src/App.java
@@ -0,0 +1,9 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Tabela de multiplicação de X");
+        int numero = 3;
+        for(int i = 0; i <= 10; i++){
+            System.out.println(numero + "x" + i + "=" + (numero*i));
+        }
+    }
+}
